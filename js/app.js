00e602f4c745ad76a3b1fba299889b9ea3e66e9e
glitch-prog <itgeniusofblr@gmail.com>
2021-11-12 11:15:48 +0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const funcs = document.querySelector('.funcs');
+const nums = document.querySelector('.nums');
+const output = document.querySelector('p');
+const rez = document.querySelector('.do');
+const AC = document.querySelector('.clearAll');
+const L = document.querySelector('.clear');
+
+AC.addEventListener('click', (event) => {
+  console.log('clearAll');
+  output.textContent = '';
+});
+
+L.addEventListener('click', (event) => {
+  console.log('deleted');
+  output.textContent = output.textContent.slice(
+    0,
+    output.textContent.length - 1
+  );
+});
+
+nums.addEventListener('click', (event) => {
+  if (event.target.classList == 'num') {
+    console.log('+');
+    output.textContent += event.target.textContent;
+  }
+});
+
+funcs.addEventListener('click', (event) => {
+  if (event.target.classList == 'func') {
+    console.log('_');
+    output.textContent += event.target.textContent;
+  }
+});
+
+rez.addEventListener('click', (event) => {
+  let result = eval(output.textContent);
+  output.textContent = result;
+});
+
+console.log(output.textContent);
+console.log(funcs);
+console.log(nums);
