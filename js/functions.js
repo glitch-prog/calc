37213b3429992f7458185104aee55b9574009261
glitch-prog <itgeniusofblr@gmail.com>
2021-11-15 15:33:46 +0300

    functions "to extent" has been added

--- js/functions.js
+++ js/functions.js
@@ -6,6 +6,11 @@ function getResult() {
   let counter2 = 0;
   let counter3 = 0;
   let counter4 = 0;
+  let counter5 = 0;
+  let counter6 = 0;
+  let counter7 = 0;
+  let counter8 = 0;
+  let counter9 = 0;
   let str = output.textContent;
   let arr = str.split(' ');
   console.log(arr);
@@ -21,8 +26,44 @@ function getResult() {
       counter3++;
     } else if (el == '!') {
       counter4++;
+    } else if (el == '^2') {
+      counter5++;
+    } else if (el == '^3') {
+      counter6++;
+    } else if (el == '^') {
+      counter7++;
     }
   });
+
+  for (let i = 0; i < counter7; i++) {
+    let w = arr.indexOf('^');
+    console.log(w);
+    arr[w - 1] = inN(arr[w - 1], arr[w + 1]);
+    arr[w - 1] = arr[w - 1].toString();
+    console.log(arr);
+    arr.splice(w, 2);
+    console.log(arr);
+  }
+
+  for (let i = 0; i < counter6; i++) {
+    let c = arr.indexOf('^3');
+    console.log(c);
+    arr[c - 1] = cube(arr[c - 1]);
+    arr[c - 1] = arr[c - 1].toString();
+    console.log(arr);
+    arr.splice(c, 2);
+    console.log(arr);
+  }
+
+  for (let i = 0; i < counter5; i++) {
+    let s = arr.indexOf('^2');
+    console.log(s);
+    arr[s - 1] = sqr(arr[s - 1]);
+    arr[s - 1] = arr[s - 1].toString();
+    console.log(arr);
+    arr.splice(s, 2);
+    console.log(arr);
+  }
 
   for (let i = 0; i < counter4; i++) {
     let f = arr.indexOf('!');
@@ -33,6 +74,7 @@ function getResult() {
     arr.splice(f - 1, 2);
     console.log(arr);
   }
+
   for (let i = 0; i < counter3; i++) {
     let l = arr.indexOf('/');
     console.log(i);
@@ -77,6 +119,22 @@ function getResult() {
 
 function factorial(n) {
   return n ? n * factorial(n - 1) : 1;
+}
+
+function sqr(n) {
+  return n ? n * n : 1;
+}
+
+function cube(n) {
+  return n ? n * n * n : 1;
+}
+
+function inN(n, m) {
+  let result = 1;
+  for (let i = 0; i < m; i++) {
+    result *= n;
+  }
+  return result;
 }
 
 export { getResult };
